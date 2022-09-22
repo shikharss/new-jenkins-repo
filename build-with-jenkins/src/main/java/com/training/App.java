abbe8790841bea27cc39a52a7370bc74518545c1
AD18855 <AD18855@IND-5CG2216WSS.ctl.intranet>
2022-09-22 12:44:56 +0530

    New Change IN Jenkins

---
+++ build-with-jenkins/src/main/java/com/training/App.java
@@ -0,0 +1,15 @@
+package com.training;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        String[] names = {"Ram", "Shyam", "Vicky"};
+        
+        for(String eachName : names) {
+        	System.out.println(eachName);
+        }
+    }
+}
